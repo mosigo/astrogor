fe410112b7064406390dfd72e1b527d363527b0c
Olga Bolshakova <mosigo@sirius.online>
2022-03-09 00:11:11 +0300

    Пробуем на все IP Яндекса отдавать 404 для вызовов транзитов, чтобы как-то приглушить робота.

--- main_app.py
+++ main_app.py
@@ -10,11 +10,35 @@ from datetime import datetime, timedelta
 
 from transliterate import translit
 
+from logging.config import dictConfig
+
 from app.app_sf import generate_full_card, generate_card
 from app.app_transit import generate_transit, generate_full_transit, get_nearest_transit_connections
 from ext.sf_geocoder import DefaultSFGeocoder
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
+
+
+def print_real_ip(method_name: str) -> str:
+    ip = request.headers.get('X-Real-IP', '')
+    app.logger.info(f'Метод {method_name} вызван с IP = {ip}.')
+    return ip
 
 
 def get_date_param_value(param_name: str, param_default_value: datetime) -> datetime:
@@ -43,91 +67,91 @@ def get_bool_param_value(param_name: str, param_default_value: bool) -> bool:
 
 @app.route('/fd', methods=['GET'])
 def fd():
-    print('fd', request.headers.get('X-Real-IP'))
+    print_real_ip('fd')
     return redirect('/card')
 
 
 @app.route('/', methods=['GET'])
 def index():
-    print('/', request.headers.get('X-Real-IP'))
+    print_real_ip('/')
     return render_template('index.html')
 
 
 @app.route('/edu', methods=['GET'])
 def edu():
-    print('edu', request.headers.get('X-Real-IP'))
+    print_real_ip('edu')
     return render_template('edu.html')
 
 
 @app.route('/contacts', methods=['GET'])
 def contacts():
-    print('contacts', request.headers.get('X-Real-IP'))
+    print_real_ip('contacts')
     return render_template('contacts.html')
 
 
 @app.route('/books', methods=['GET'])
 def books():
-    print('books', request.headers.get('X-Real-IP'))
+    print_real_ip('books')
     return render_template('books.html')
 
 
 @app.route('/videos', methods=['GET'])
 def videos():
-    print('videos', request.headers.get('X-Real-IP'))
+    print_real_ip('videos')
     return render_template('videos.html')
 
 
 @app.route('/astro', methods=['GET'])
 def astro():
-    print('astro', request.headers.get('X-Real-IP'))
+    print_real_ip('astro')
     return render_template('astro.html')
 
 
 @app.route('/others', methods=['GET'])
 def others():
-    print('others', request.headers.get('X-Real-IP'))
+    print_real_ip('others')
     return render_template('others.html')
 
 
 @app.route('/consultations', methods=['GET'])
 def consultations():
-    print('consultations', request.headers.get('X-Real-IP'))
+    print_real_ip('consultations')
     return render_template('consultations.html')
 
 
 @app.route('/news', methods=['GET'])
 def news():
-    print('news', request.headers.get('X-Real-IP'))
+    print_real_ip('news')
     return render_template('news.html')
 
 
 @app.route('/courses/fd', methods=['GET'])
 def course_fd():
-    print('courses/fd', request.headers.get('X-Real-IP'))
+    print_real_ip('courses/fd')
     return render_template('courses/fd.html')
 
 
 @app.route('/courses/astro', methods=['GET'])
 def course_astro():
-    print('courses/astro', request.headers.get('X-Real-IP'))
+    print_real_ip('courses/astro')
     return render_template('courses/astro.html')
 
 
 @app.route('/courses/karma', methods=['GET'])
 def course_karma():
-    print('courses/karma', request.headers.get('X-Real-IP'))
+    print_real_ip('courses/karma')
     return render_template('courses/karma.html')
 
 
 @app.route('/courses/palmistry', methods=['GET'])
 def course_palmistry():
-    print('courses/palmistry', request.headers.get('X-Real-IP'))
+    print_real_ip('courses/palmistry')
     return render_template('courses/palmistry.html')
 
 
 @app.route('/download-card', methods=['GET'])
 def download_card():
-    print('download-card', request.headers.get('X-Real-IP'))
+    print_real_ip('download-card')
     fio, birthday, city, age_units = get_card_params()
 
     birthday_unified = birthday.strftime('%Y-%m-%d %H:%M')
@@ -148,7 +172,7 @@ def get_card_params():
 
 @app.route('/card', methods=['GET'])
 def create_card():
-    print('card', request.headers.get('X-Real-IP'))
+    print_real_ip('card')
     fio, birthday, city, age_units = get_card_params()
 
     birthday_as_str = birthday.strftime('%d.%m.%Y %H:%M')
@@ -204,7 +228,25 @@ def unquote_bool_param(param_name: str, param_value: bool) -> str:
 
 @app.route('/transit', methods=['GET'])
 def create_transit():
-    print('transit', request.headers.get('X-Real-IP'))
+    ip = print_real_ip('transit')
+    # пытаемся отдать роботу Яндекса 404 — временная мера
+    if request.args.get('birthday') and ip in ['141.8.142.67', '141.8.142.78', '141.8.142.94', '213.180.203.12',
+                                               '213.180.203.175', '213.180.203.183', '213.180.203.20',
+                                               '213.180.203.249', '213.180.203.252', '213.180.203.5', '213.180.203.66',
+                                               '213.180.203.67', '213.180.203.85', '213.180.203.89', '213.180.203.91',
+                                               '213.180.203.96', '5.255.253.113', '5.255.253.130', '5.255.253.148',
+                                               '5.255.253.164', '5.255.253.165', '5.255.253.177', '5.45.207.112',
+                                               '5.45.207.115', '5.45.207.126', '5.45.207.133', '5.45.207.142',
+                                               '5.45.207.65', '5.45.207.71', '5.45.207.72', '87.250.224.101',
+                                               '87.250.224.103', '87.250.224.138', '87.250.224.139', '87.250.224.146',
+                                               '87.250.224.16', '87.250.224.181', '87.250.224.189', '87.250.224.190',
+                                               '87.250.224.193', '87.250.224.198', '87.250.224.21', '87.250.224.27',
+                                               '87.250.224.40', '87.250.224.48', '87.250.224.52', '87.250.224.60',
+                                               '87.250.224.70', '87.250.224.74', '87.250.224.76', '95.108.213.10',
+                                               '95.108.213.15', '95.108.213.24', '95.108.213.26', '95.108.213.27',
+                                               '95.108.213.4', '95.108.213.41', '95.108.213.69', '95.108.213.71']:
+        return page_not_found(None)
+
     fio, birthday_dt, city, transit_dt, cur_city, show_source, show_source_to_transit, show_transit = \
         get_transit_params()
 
@@ -243,7 +285,8 @@ def create_transit():
     fd_link = '/card?' + \
               unquote_str_param('fio', fio) + unquote_str_param('birthday', birthday) + unquote_str_param('city', city)
 
-    planet_to_connection, planet_to_view = get_nearest_transit_connections(geocoder, birthday_dt, city, transit_dt, cur_city)
+    planet_to_connection, planet_to_view = get_nearest_transit_connections(geocoder, birthday_dt, city, transit_dt,
+                                                                           cur_city)
     planet_to_connection_res = []
     for planet, (planet_to, dt, _) in planet_to_connection.items():
         dt_as_str = dt.strftime('%d.%m.%Y %H:%M')
@@ -265,7 +308,7 @@ def create_transit():
 
 @app.route('/download-tr', methods=['GET'])
 def download_transit():
-    print('download-tr', request.headers.get('X-Real-IP'))
+    print_real_ip('download-tr')
     fio, birthday_dt, city, transit_dt, cur_city, show_source, show_source_to_transit, show_transit = \
         get_transit_params()
 
